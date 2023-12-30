9b7b78a8c4c9187255b51b54f19a4d1da7f72e00
Tabitha <53835819+TabbyCat444@users.noreply.github.com>
2023-12-29 19:40:26 -0600

    Create TwoIntegerLab.py

---
+++ TwoIntegerLab.py
@@ -0,0 +1,17 @@
+# BASIC MATH LAB #
+
+# Take user input of two integers
+user_num = int(input('Enter integer:\n'))
+
+print('You entered:', user_num)
+# Outputs the square of the first integer
+print(user_num, 'squared is', user_num * user_num)
+# Outputs the cube of the first integer
+print('And', user_num, 'cubed is', user_num * user_num * user_num, '!!')
+
+user_num2 = int(input('Enter another integer:\n'))
+
+# Outputs the sum of the two integers
+print(user_num, '+', user_num2, 'is', user_num + user_num2)
+# Outputs the product of the two integers
+print(user_num, '*', user_num2, 'is', user_num * user_num2)
